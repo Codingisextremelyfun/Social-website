a124c3e5a5d83ba51d96307adebed9fee7ccd2b7
Codingisextremelyfun <114877978+Codingisextremelyfun@users.noreply.github.com>
2023-09-19 17:06:06 -0700

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,4 +1,14 @@
-
+// Your web app's Firebase configuration
+const firebaseConfig = {
+      apiKey: "AIzaSyBsxD2PJggF_JHXFkGo-LjscgbMDGNPk2s",
+      authDomain: "socialwebsite-14c1d.firebaseapp.com",
+      databaseURL: "https://socialwebsite-14c1d-default-rtdb.firebaseio.com",
+      projectId: "socialwebsite-14c1d",
+      storageBucket: "socialwebsite-14c1d.appspot.com",
+      messagingSenderId: "844822186770",
+      appId: "1:844822186770:web:3f313dc160c6ab63a3c1cb"
+    };
+    
 //ADD YOUR FIREBASE LINKS HERE
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
@@ -8,3 +18,11 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
       //End code
       });});}
 getData();
+
+function removeUser()
+{
+    user_name = document.getElementById("user_name").value;
+
+    localStorage.removeItem("user_name", user_name);
+    window.location = "index.html";
+}
